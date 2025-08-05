b7abcdf69bb4cce07a9329d6e9da4e6f7f515ffd
RaviPrajapat007 <120958642+RaviPrajapat007@users.noreply.github.com>
2025-08-05 23:34:36 +0530

    Initial commit

--- src/assets/Components/Portfolio.jsx
+++ src/assets/Components/Portfolio.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from "./Portfolio.module.css"
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className={styles.portfolio}>
+      <div className={styles.uicontainer}>
+         <h1>UI/UX Designs</h1>
+
+      </div>
+    </div>
   )
 }
 
